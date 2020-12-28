a3ff86472e2187d731fe42495b44ad2b92724747
anishmoktan <anish.tmngo@gmail.com>
2020-12-28 13:43:34 -0500

    can create a normal website

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ app.get("/", function(req,res){
 
 });
 
+app.get("/about", function(req,res){
 
+  res.render("about", {aboutContent: aboutContent}); //passing the variable of ejs from home.ejs and forming a key value pair
+
+});
+
+app.get("/contact", function(req,res){
+
+  res.render("contact", {contactContent: contactContent}); //passing the variable of ejs from home.ejs and forming a key value pair
+
+});
 
 
 
